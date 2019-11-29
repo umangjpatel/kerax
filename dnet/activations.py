f87eb76893a197465fdcc0712f2b4b0d5dd34486
umangjpatel <umangpatel1947@gmail.com>
2019-11-29 15:21:09 +0530

    v0.7.1 : Added Mish and softplus activation functions

--- dnet/activations.py
+++ dnet/activations.py
@@ -10,3 +10,18 @@ def sigmoid(z: tensor.array) -> tensor.array:
 @jit
 def relu(z: tensor.array) -> tensor.array:
     return tensor.maximum(0.0, z)
+
+
+@jit
+def tanh(z: tensor.array) -> tensor.array:
+    return tensor.tanh(z)
+
+
+@jit
+def softplus(z: tensor.array) -> tensor.array:
+    return tensor.log(1 + tensor.exp(z))
+
+
+@jit
+def mish(z: tensor.array) -> tensor.array:
+    return z * tanh(softplus(z))
