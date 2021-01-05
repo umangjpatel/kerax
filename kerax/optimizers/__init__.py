25ff0cf69e5310d892268785a8964b350ee73a46
umangjpatel <umangpatel1947@gmail.com>
2021-01-05 17:34:30 +0530

    Added more optimizers from jax.experimental.stax.optimizers module...

--- kerax/optimizers/__init__.py
+++ kerax/optimizers/__init__.py
@@ -1,8 +1,17 @@
 from jax.experimental.optimizers import OptimizerState
-from jax.experimental.optimizers import sgd as SGD
+from jax.experimental.optimizers import adagrad as Adagrad
+from jax.experimental.optimizers import adam as Adam
+from jax.experimental.optimizers import adamax as Adamax
 from jax.experimental.optimizers import rmsprop as RMSProp
+from jax.experimental.optimizers import sgd as SGD
+from jax.experimental.optimizers import sm3 as SM3
 
 __all__ = [
+    "Adam",
+    "Adagrad",
+    "Adamax",
     "OptimizerState",
-    "SGD"
+    "RMSProp",
+    "SGD",
+    "SM3"
 ]
